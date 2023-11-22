1d14ef833a72783a0355b7874ff3310f05688cd6
AllaMB <126911591+AllaMB@users.noreply.github.com>
2023-11-22 19:26:11 +0100

    implement interactive plan highlighting

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { planData } from './components/planData/data';
 import { PlanCard} from './components/planCard/index';
 import styles from './assets/styles/app.module.scss';
 
 function App() {
+  const [selectedPlan, setSelectedPlan] = useState(null);
+
+  const handlePlanClick = (index) => {
+    setSelectedPlan(index === selectedPlan ? null : index);
+  };
+
   return (
       <div className={styles.app}>
-        {planData.map((plan) => (
+        {planData.map((plan, index) => (
           <PlanCard
           key={plan.name}
           name={plan.name}
           price={plan.price}
           speed={plan.speed}
           theme={plan.theme}
+          selected={index === selectedPlan}
+          onClick={() => handlePlanClick(index)}
           />
         ))}
       </div>
